a5d8b809178b6d02a83d426e8d8025af18c8d9cd
Jelani Thompson <jelani.thompson@outlook.com>
2017-05-08 13:11:17 -0400

    getAllIdsTest

---
+++ snippet/test/getAllIds.test.js
@@ -0,0 +1,16 @@
+// const getAllIds = require("../index.js").getAllIds;
+
+// describe("getAllIds()", () => {
+//   // Record arguments / return value
+//   const getAllIdsSpy1 = sinon.spy();
+//   const result = getAllIds(getAllIdsSpy1);
+
+//   // it("should return state", () => {
+//   //     expect(typeof result).toBe("object");
+//   // });
+
+//   // Still needs to be tested
+// //   it("should ensure ids is an array", () => {
+// //       expect(typeof ids).toBe("array");
+// //   });
+// });
